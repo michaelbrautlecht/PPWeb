b806f2597ee839542164d69cc2eefe14e2df2a23
michaelbrautlecht <brautlecht@weroca.de>
2026-01-05 07:13:49 +0100

    Add Auth

--- PPWebApplication/PPWebApplication/obj/Debug/net8.0/PPWebApplication.AssemblyInfo.cs
+++ PPWebApplication/PPWebApplication/obj/Debug/net8.0/PPWebApplication.AssemblyInfo.cs
@@ -11,10 +11,11 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.AspNetCore.Identity.UI.UIFrameworkAttribute("Bootstrap5")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("PPWebApplication")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4bb4c888cdc159012f445d4122616de902a5a0d4")]
 [assembly: System.Reflection.AssemblyProductAttribute("PPWebApplication")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PPWebApplication")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
